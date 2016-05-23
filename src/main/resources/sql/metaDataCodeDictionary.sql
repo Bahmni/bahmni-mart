1b09679fe2a04b5d24c56f1b2405d05a9bc9b5c1
Shruthip <shruthi.pitta@gmail.com>
2016-05-23 16:02:16 +0530

    Shruthi|Changed the source name to EndTb Export

--- src/main/resources/sql/metaDataCodeDictionary.sql
+++ src/main/resources/sql/metaDataCodeDictionary.sql
@@ -1,5 +1,5 @@
 select cv.concept_full_name, crtv.code
 from concept_view cv,concept_reference_term_map_view crtv
 where cv.concept_id = crtv.concept_id
-and crtv.concept_reference_source_name = 'SNOMED CT'
+and crtv.concept_reference_source_name = 'EndTB-Export'
 and crtv.concept_map_type_name='SAME-AS';
