b24ba1f1d34107c33efa66fca7b7b94705b1b4e0
bharatak <bharatak@thoughtworks.com>
2016-06-05 13:52:59 +0530

    Bharat| Updated headers of Metadata code dictionary

--- src/main/resources/sql/metaDataCodeDictionary.sql
+++ src/main/resources/sql/metaDataCodeDictionary.sql
@@ -1,4 +1,4 @@
-select cv.concept_full_name as question, crtv.code as question_code,cn.name as answer,crtv_answer.code as answer_code, ca.sort_weight
+select CONCAT('\"',cv.concept_full_name,'\"') as question, crtv.code as question_code,CONCAT('\"',cn.name,'\"') as answer,crtv_answer.code as answer_code
 from concept_view cv
   inner join concept_reference_term_map_view crtv on cv.concept_id=crtv.concept_id and crtv.concept_reference_source_name = 'EndTB-Export' and crtv.concept_map_type_name='SAME-AS'
   LEFT OUTER JOIN concept_answer ca on cv.concept_id=ca.concept_id
