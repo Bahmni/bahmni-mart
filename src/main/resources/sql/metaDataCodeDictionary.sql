0ad32d96df6528795c5bd80332918235ed718168
Shireesha <bongaralashireesha@gmail.com>
2016-06-29 14:13:22 +0530

    Shireesha | Changing header in metadatacodedictionary

--- src/main/resources/sql/metaDataCodeDictionary.sql
+++ src/main/resources/sql/metaDataCodeDictionary.sql
@@ -1,4 +1,4 @@
-select CONCAT('\"',cv.concept_full_name,'\"') as fully_specified_name,CONCAT('\"',cv.concept_short_name,'\"') as question,CONCAT('\"',cv.description,'\"') as description, crtv.code as question_code, CONCAT('\"',cn.name,'\"') as answer,crtv_answer.code as answer_code
+select CONCAT('\"',cv.concept_full_name,'\"') as fully_specified_name,CONCAT('\"',cv.concept_short_name,'\"') as question,CONCAT('\"',cv.description,'\"') as description, crtv.code as question_header, CONCAT('\"',cn.name,'\"') as answer,crtv_answer.code as answer_code
 from concept_view cv
   inner join concept_reference_term_map_view crtv on cv.concept_id=crtv.concept_id and crtv.concept_reference_source_name = 'EndTB-Export' and crtv.concept_map_type_name='SAME-AS'
   JOIN concept_answer ca on cv.concept_id=ca.concept_id
