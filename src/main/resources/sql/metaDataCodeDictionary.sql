2059cd14bb584e1061b4b950a1cde2b8967e4c09
bharatak <bharatak@thoughtworks.com>
2016-06-30 09:53:08 +0530

    Bharat| Fixed the issue for incomplete rename of question_code to question_header

--- src/main/resources/sql/metaDataCodeDictionary.sql
+++ src/main/resources/sql/metaDataCodeDictionary.sql
@@ -4,4 +4,4 @@ from concept_view cv
   JOIN concept_answer ca on cv.concept_id=ca.concept_id
   LEFT OUTER JOIN concept_name cn on ca.answer_concept=cn.concept_id and cn.concept_name_type='FULLY_SPECIFIED'
   LEFT OUTER JOIN concept_reference_term_map_view  crtv_answer on ca.answer_concept = crtv_answer.concept_id and crtv_answer.concept_map_type_name='SAME-AS' and crtv_answer.concept_reference_source_name='EndTB-Export'
-ORDER BY question asc,question_code asc,sort_weight asc;
+ORDER BY question asc,question_header asc,sort_weight asc;
