e52f15e46b8d7f81620d661cafaa8863b931daa4
Deepak Sharma <deepaucksharma@gmail.com>
2016-06-30 15:36:35 +0530

    #1969 Deepak, Shiresha - remove special characters in the  description for metadataCodeDictionary

--- src/main/resources/sql/metaDataCodeDictionary.sql
+++ src/main/resources/sql/metaDataCodeDictionary.sql
@@ -1,7 +1,27 @@
-select CONCAT('\"',cv.concept_full_name,'\"') as fully_specified_name,CONCAT('\"',cv.concept_short_name,'\"') as question,CONCAT('\"',cv.description,'\"') as description, crtv.code as question_header, CONCAT('\"',cn.name,'\"') as answer,crtv_answer.code as answer_code
-from concept_view cv
-  inner join concept_reference_term_map_view crtv on cv.concept_id=crtv.concept_id and crtv.concept_reference_source_name = 'EndTB-Export' and crtv.concept_map_type_name='SAME-AS'
-  JOIN concept_answer ca on cv.concept_id=ca.concept_id
-  LEFT OUTER JOIN concept_name cn on ca.answer_concept=cn.concept_id and cn.concept_name_type='FULLY_SPECIFIED'
-  LEFT OUTER JOIN concept_reference_term_map_view  crtv_answer on ca.answer_concept = crtv_answer.concept_id and crtv_answer.concept_map_type_name='SAME-AS' and crtv_answer.concept_reference_source_name='EndTB-Export'
-ORDER BY question asc,question_header asc,sort_weight asc;
+SELECT  CONCAT('\"', cv.concept_full_name, '\"')  AS fully_specified_name,
+        CONCAT('\"', cv.concept_short_name, '\"') AS question,
+        CONCAT( '\"',
+                REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(cv.description, '"', ' '), '\t', ' '), '\n', ' '), '\r', ' '), ',', ' '),
+                '\"')        AS description,
+        crtv.code                                 AS question_code,
+        CONCAT('\"', cn.name, '\"')               AS answer,
+        crtv_answer.code                          AS answer_code
+FROM    concept_view cv
+  INNER JOIN
+  concept_reference_term_map_view crtv ON   cv.concept_id = crtv.concept_id AND
+                                            crtv.concept_reference_source_name = 'EndTB-Export' AND
+                                            crtv.concept_map_type_name = 'SAME-AS'
+  JOIN
+  concept_answer ca ON cv.concept_id = ca.concept_id
+  LEFT OUTER JOIN
+  concept_name cn ON  ca.answer_concept = cn.concept_id AND
+                      cn.concept_name_type = 'FULLY_SPECIFIED'
+  LEFT OUTER JOIN
+  concept_reference_term_map_view crtv_answer ON  ca.answer_concept = crtv_answer.concept_id AND
+                                                  crtv_answer.concept_map_type_name = 'SAME-AS' AND
+                                                  crtv_answer.concept_reference_source_name = 'EndTB-Export'
+ORDER BY  question ASC,
+  question_code ASC,
+  sort_weight ASC;
+
+
