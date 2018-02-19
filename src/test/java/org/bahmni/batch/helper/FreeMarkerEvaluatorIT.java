6c9e7c1fdd62418aae4435b17008139c41be4348
sumanmaity112 <suman.maity112@gmail.com>
2018-02-19 23:48:32 +0530

    Suman | Update springboot, gradle and nebula.ospackage vesrion to latest

--- src/test/java/org/bahmni/batch/helper/FreeMarkerEvaluatorIT.java
+++ src/test/java/org/bahmni/batch/helper/FreeMarkerEvaluatorIT.java
@@ -7,14 +7,14 @@ import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import static org.junit.Assert.assertEquals;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = Application.class)
+@SpringBootTest(classes = Application.class)
 @TestPropertySource(locations="classpath:test.properties")
 @Ignore
 public class FreeMarkerEvaluatorIT {
