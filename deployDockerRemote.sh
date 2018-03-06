f9df3707c621c1bbb5e4d9e4899ff7a6bc65a3eb
sudhamshk <sudhamsh543@gmail.com>
2018-03-06 13:09:39 +0530

    Sudhamsh, Prasanna | #10 | add wget before execution

--- deployDockerRemote.sh
+++ deployDockerRemote.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+sshpass -p $DEPLOY_PASSWORD ssh $DEPLOY_USERNAME@$DEPLOY_HOST -o stricthostkeychecking=no 'wget https://raw.githubusercontent.com/bahmni-msf/bahmni-mart/master/deployDocker.sh -O deployDocker.sh'
 sshpass -p $DEPLOY_PASSWORD ssh $DEPLOY_USERNAME@$DEPLOY_HOST -o stricthostkeychecking=no 'bash deployDocker.sh'
 if [ "$?" != 0 ]; then
    exit "$?"
