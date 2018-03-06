8afe7c000f757d3047e6720d3b9301d282f9a940
sudhamshk <sudhamsh543@gmail.com>
2018-03-06 15:36:47 +0530

    Sudhamsh, Prasanna | #10 | remove exited container

--- deployDocker.sh
+++ deployDocker.sh
@@ -3,5 +3,5 @@
 sudo docker pull anallytics/bahmni-mart
 sudo wget https://raw.githubusercontent.com/bahmni-msf/bahmni-mart/master/conf/bahmni-mart.conf -O bahmni-mart.conf
 sudo docker stop mart
-sudo docker rm $(docker ps -a -f status=exited -q)
+sudo docker rm mart
 sudo docker run --privileged=true --name mart -p 4432:5432 -v $(pwd)/bahmni-mart.conf:/opt/bahmni-mart/conf/bahmni-mart.conf -i bahmni-mart
