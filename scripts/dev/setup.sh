0332c35917d3d56faca014940f72656f7ccaf0d1
sumanmaity112 <suman.maity112@gmail.com>
2018-03-12 12:43:20 +0530

    Suman | #00 | Fix path for dev related scripts

--- scripts/dev/setup.sh
+++ scripts/dev/setup.sh
@@ -4,8 +4,8 @@ echo "-----------SETUP IS STARTED-----------"
 PATH_OF_CURRENT_SCRIPT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 PATH_OF_GIT_HOOK_DIR="${PATH_OF_CURRENT_SCRIPT}/../../.git/hooks/"
 
-ln -s ${PATH_OF_CURRENT_SCRIPT}/dev/pre-push.sh ${PATH_OF_GIT_HOOK_DIR}/pre-push && chmod +x ${PATH_OF_GIT_HOOK_DIR}/pre-push
-ln -s ${PATH_OF_CURRENT_SCRIPT}/dev/commit-msg ${PATH_OF_GIT_HOOK_DIR}/commit-msg && chmod +x ${PATH_OF_GIT_HOOK_DIR}/commit-msg
+ln -s ${PATH_OF_CURRENT_SCRIPT}/pre-push.sh ${PATH_OF_GIT_HOOK_DIR}/pre-push && chmod +x ${PATH_OF_GIT_HOOK_DIR}/pre-push
+ln -s ${PATH_OF_CURRENT_SCRIPT}/commit-msg ${PATH_OF_GIT_HOOK_DIR}/commit-msg && chmod +x ${PATH_OF_GIT_HOOK_DIR}/commit-msg
 
 if [ $? != 0 ]
 then
