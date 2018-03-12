370fa0e5e044866c8640b266e9619889f013e689
sudhamshk <sudhamsh543@gmail.com>
2018-03-12 12:54:30 +0530

    Sudhamsh | #10 | edit Repo name in build docker

--- scripts/buildDocker.sh
+++ scripts/buildDocker.sh
@@ -5,16 +5,16 @@ export COMMIT=${TRAVIS_COMMIT::8}
 docker login -u $DOCKER_USER -p $DOCKER_PASS
 export JAVAREPO=anallytics/bahmni-mart
 export TAG=$(if [ "$TRAVIS_BRANCH" == "master" ]; then echo "latest"; else echo $TRAVIS_BRANCH ; fi)
-docker build -f JavaDockerfile -t $REPO:$COMMIT .
-docker tag $JAVAREPO:$COMMIT $REPO:$TAG
-docker tag $JAVAREPO:$COMMIT $REPO:mart-$TRAVIS_BUILD_NUMBER
+docker build -f JavaDockerfile -t $JAVAREPO:$COMMIT .
+docker tag $JAVAREPO:$COMMIT $JAVAREPO:$TAG
+docker tag $JAVAREPO:$COMMIT $JAVAREPO:mart-$TRAVIS_BUILD_NUMBER
 docker push $JAVAREPO
 
 export POSTGRESREPO=anallytics/postgres
 export TAG=$(if [ "$TRAVIS_BRANCH" == "master" ]; then echo "latest"; else echo $TRAVIS_BRANCH ; fi)
-docker build -f PostgresqlDockerfile -t $REPO:$COMMIT .
-docker tag $POSTGRESREPO:$COMMIT $REPO:$TAG
-docker tag $POSTGRESREPO:$COMMIT $REPO:mart-$TRAVIS_BUILD_NUMBER
+docker build -f PostgresqlDockerfile -t $POSTGRESREPO:$COMMIT .
+docker tag $POSTGRESREPO:$COMMIT $POSTGRESREPO:$TAG
+docker tag $POSTGRESREPO:$COMMIT $POSTGRESREPO:postgres-$TRAVIS_BUILD_NUMBER
 docker push $POSTGRESREPO
 
 if [ "$?" != 0 ]; then
