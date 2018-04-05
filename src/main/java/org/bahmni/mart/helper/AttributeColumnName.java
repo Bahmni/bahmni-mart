999e08dae00358d83e50732ab96bcf21e86d7a3f
Mahitha Arveti <mahitha.arveti@gmail.com>
2018-04-05 12:18:13 +0530

    Mahitha | #36 | pivoted patient_identifier table

--- src/main/java/org/bahmni/mart/helper/AttributeColumnName.java
+++ src/main/java/org/bahmni/mart/helper/AttributeColumnName.java
@@ -4,7 +4,8 @@ public enum AttributeColumnName {
     person_attribute_type("format"),
     surgical_appointment_attribute_type("format"),
     visit_attribute_type("datatype"),
-    provider_attribute_type("datatype");
+    provider_attribute_type("datatype"),
+    patient_identifier_type("format");
 
     private String datatype;
 
