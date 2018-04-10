36af0fa89110ec3761e3d165ccf4e5f16292b019
swetham <swetham@thoughtworks.com>
2018-04-10 14:32:37 +0530

    Suman, Swetha | #16 | Add shebang line to scripts

--- scripts/addTask.sh
+++ scripts/addTask.sh
@@ -1,2 +1,3 @@
+#!/usr/bin/env bash
 app register --name bahmni-mart --type task --uri file:///opt/bahmni-mart/bahmni-mart.jar
 task create create-bahmni-mart --definition bahmni-mart\ --spring.config.location=/opt/bahmni-mart/conf/\ --spring.profiles.active=prod
