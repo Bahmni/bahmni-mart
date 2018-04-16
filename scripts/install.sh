89f4d13e58b30256ed5fbf804d2c3c5adf167436
sumanmaity112 <suman.maity112@gmail.com>
2018-04-16 23:22:04 +0530

    Suman | #16 | Create /var/log/bahmni-mart directory as part of install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+mkdir -p /var/log/bahmni-mart
 wget -O bahmni-mart.tar.gz "https://s3.ap-south-1.amazonaws.com/bahmni-mart/travis-builds/bahmni-mart.tar.gz"
 mkdir -p /opt/bahmni-mart && tar -xvzf bahmni-mart.tar.gz -C /opt/bahmni-mart
 rm -rf bahmni-mart.tar.gz
