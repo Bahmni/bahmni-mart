2bf2d04eabc2cf71c30fa45b50b8d20254bfe9d7
Prasanna <lakshmip@thoughtworks.com>
2018-04-16 12:12:13 +0530

    Prasanna | #42 | Fix foriegn key issue with multi level seperate tables

--- src/test/java/org/bahmni/mart/helper/FreeMarkerEvaluatorIT.java
+++ src/test/java/org/bahmni/mart/helper/FreeMarkerEvaluatorIT.java
@@ -34,6 +34,7 @@ public class FreeMarkerEvaluatorIT extends AbstractBaseBatchIT {
 
         BahmniForm child = new BahmniForm();
         child.setParent(parent);
+        child.setRootForm(parent);
         child.setFormName(new Concept(10, "Systolic", 1));
         child.setDepthToParent(1);
 
@@ -87,7 +88,7 @@ public class FreeMarkerEvaluatorIT extends AbstractBaseBatchIT {
 
 
         String sql = dynamicObsQuery.evaluate("obsWithParentSql.ftl", child);
-        assertEquals("SELECT obs0.obs_id , obs3.obs_id as parent_obs_id FROM obs obs0 INNER JOIN obs obs1 " +
+        assertEquals("SELECT obs0.obs_id , obs1.obs_id as parent_obs_id FROM obs obs0 INNER JOIN obs obs1 " +
                 "on (obs1.obs_id = obs0.obs_group_id and obs1.voided = 0) INNER JOIN obs obs2 on (obs2.obs_id = " +
                 "obs1.obs_group_id and obs2.voided = 0) INNER JOIN obs obs3 on (obs3.obs_id = obs2.obs_group_id and " +
                 "obs3.voided = 0) WHERE obs0.concept_id =12 AND obs0.voided = 0 AND obs3.concept_id =10", sql.trim());
