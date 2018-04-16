78b013d1f4a762fe8135cba3c1eeb43941a82b97
sumanmaity112 <suman.maity112@gmail.com>
2018-04-16 22:09:48 +0530

    Suman | #16 | Hardcode BAHMNI_MART_ZIP_URL in script

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-wget -O bahmni-mart.tar.gz ${BAHMNI_MART_ZIP_URL}
-mkdir -p /opt/bahmni-mart && tar -xzf bahmni-mart.tar.gz -C /opt/bahmni-mart
+wget -O bahmni-mart.tar.gz "https://s3.ap-south-1.amazonaws.com/bahmni-mart/travis-builds/bahmni-mart.tar.gz"
+mkdir -p /opt/bahmni-mart && tar -xvzf bahmni-mart.tar.gz -C /opt/bahmni-mart
 rm -rf bahmni-mart.tar.gz
