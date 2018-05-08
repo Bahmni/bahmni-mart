8aa776cb41df3fa7cab5e761ea9c1c33198e72ac
Prasanna <lakshmip@thoughtworks.com>
2018-05-08 10:32:46 +0530

    Prasanna | #63 | change column names for bed managment locations view

--- src/main/resources/viewSql/locationWiseDischarge.sql
+++ src/main/resources/viewSql/locationWiseDischarge.sql
@@ -2,8 +2,8 @@ SELECT
   bpam.patient_id,
   bpam.visit_id,
   bpam.location,
-  min(bpam.date_started)              AS startDate,
-  coalesce(discharge_date(patient_id,bpam.location,date_stopped),current_date) AS dischargeDate
+  min(bpam.date_started)              AS start_date,
+  coalesce(discharge_date(patient_id,bpam.location,date_stopped),current_date) AS discharge_date
 FROM bed_patient_assignment bpam
 GROUP BY patient_id,visit_id,dischargeDate,location
 ORDER BY startDate,dischargeDate
