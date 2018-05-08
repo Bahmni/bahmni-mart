ca5e2d3e880fbd1f5d88551b7ee517e5ca93414f
Prasanna <lakshmip@thoughtworks.com>
2018-05-08 11:26:50 +0530

    Prasanna | #63 | Rename column names in bed management locations view

--- src/main/resources/viewSql/locationWiseDischarge.sql
+++ src/main/resources/viewSql/locationWiseDischarge.sql
@@ -5,5 +5,5 @@ SELECT
   min(bpam.date_started)              AS start_date,
   coalesce(discharge_date(patient_id,bpam.location,date_stopped),current_date) AS discharge_date
 FROM bed_patient_assignment bpam
-GROUP BY patient_id,visit_id,dischargeDate,location
-ORDER BY startDate,dischargeDate
+GROUP BY patient_id,visit_id,discharge_date,location
+ORDER BY start_date,discharge_date
