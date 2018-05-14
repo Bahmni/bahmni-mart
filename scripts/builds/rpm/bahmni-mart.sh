0aceee338335fd725af24734e6d26f95ebca3232
sumanmaity112 <suman.maity112@gmail.com>
2018-05-14 23:47:30 +0530

    Suman | #70 | Rename /opt/bahmni-mart/libs directory to /opt/bahmni-mart/lib

--- scripts/builds/rpm/bahmni-mart.sh
+++ scripts/builds/rpm/bahmni-mart.sh
@@ -3,5 +3,5 @@
 . /opt/bahmni-mart/conf/config-variables.conf
 
 echo "Running bahmni-mart..."
-java -jar /opt/bahmni-mart/libs/bahmni-mart-*.jar >> /opt/bahmni-mart/log/bahmni-mart.log
+java -jar /opt/bahmni-mart/lib/bahmni-mart.jar >> /opt/bahmni-mart/log/bahmni-mart.log
 echo "Done"
