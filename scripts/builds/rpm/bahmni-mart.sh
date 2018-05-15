db5fff32967ec917c2cc3064f037ac9155a10907
sumanmaity112 <suman.maity112@gmail.com>
2018-05-15 17:29:38 +0530

    Suman | #70 | Read config from /var/www/bahmni_config/bahmni-mart for bahmni-mart command

--- scripts/builds/rpm/bahmni-mart.sh
+++ scripts/builds/rpm/bahmni-mart.sh
@@ -9,7 +9,7 @@ initialize(){
 
 launch_bahmni_mart(){
     echo "Launching bahmni-mart..."
-    nohup java -jar /opt/bahmni-mart/lib/bahmni-mart.jar --spring.profiles.active="host" --spring.config.location="/opt/bahmni-mart/conf/" >> /opt/bahmni-mart/log/bahmni-mart.log 2>&1 &
+    nohup java -jar /opt/bahmni-mart/lib/bahmni-mart.jar --spring.profiles.active="host" --spring.config.location="/var/www/bahmni_config/bahmni-mart/" >> /var/log/bahmni-mart/bahmni-mart.log 2>&1 &
     echo "Done"
 }
 
