918e9dbfd44bec99956321d84521d9f8ebb3b361
sumanmaity112 <suman.maity112@gmail.com>
2018-05-15 14:21:29 +0530

    Suman | #70 | Fix the scripts command for travis build

--- scripts/travis/createZipFile.sh
+++ scripts/travis/createZipFile.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-./gradlew clean buildRPM -x check #skiping checks as we will do the same in above stage
+./gradlew clean createZip -x check #skiping checks as we will do the same in above stage
 
 exit "$?"
