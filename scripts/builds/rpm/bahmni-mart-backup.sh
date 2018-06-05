0d6f20eea46861bab35dc3b601d9dfd14ba03ed6
sumanmaity112 <suman.maity112@gmail.com>
2018-06-05 18:06:49 +0530

    Suman, Prasanna | #85 | Change script to use gpg instead of openssl

--- scripts/builds/rpm/bahmni-mart-backup.sh
+++ scripts/builds/rpm/bahmni-mart-backup.sh
@@ -3,20 +3,28 @@
 PATH_OF_CURRENT_SCRIPT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 . ${PATH_OF_CURRENT_SCRIPT}/bahmni-mart-db-details.sh
 
-read -p "Enter PEM file location (Optional, default is '~/.ssh/id_rsa'): " PUBLIC_KEY_FILE_LOCATION
+read -p "Enter recipient name(s) with comma(,) separation: " RECIPIENT_AS_CSV
 
-OUTPUT_FILE_NAME=/data/analytics/analytics_$(date +"%Y%m%d%H%M").dump
+OUTPUT_FILE_NAME=/data/analytics/analytics_$(date +"%Y%m%d%H%M").dump.gpg
+RECIPIENTS=""
 
-if [ -z "$PUBLIC_KEY_FILE_LOCATION" ]; then
-    PUBLIC_KEY_FILE_LOCATION=~/.ssh/id_rsa.pub
+echo
+
+if [ ! -z "$RECIPIENT_AS_CSV" ]; then
+    IFS=',' read -ra ADDR <<< "$RECIPIENT_AS_CSV"
+    for i in "${ADDR[@]}"; do
+        RECIPIENTS="$RECIPIENTS -r $i"
+    done
+else
+    echo "Please provide recipient name(s)"
+    exit 1;
 fi
 
 echo "Creating encrypted backup file ${OUTPUT_FILE_NAME}"
 
-pg_dump -h ${ANALYTICS_DB_HOST} -p ${POSTGRESQL_PORT} -U ${ANALYTICS_DB_USERNAME} ${ANALYTICS_DB_NAME} -c | bzip2 | openssl smime -encrypt -aes256 -binary -outform DER -out ${OUTPUT_FILE_NAME} ${PUBLIC_KEY_FILE_LOCATION}
+pg_dump -h ${ANALYTICS_DB_HOST} -p ${POSTGRESQL_PORT} -U ${ANALYTICS_DB_USERNAME} ${ANALYTICS_DB_NAME} -c | gpg ${RECIPIENTS} --always-trust --encrypt -o ${OUTPUT_FILE_NAME}
 
 EXIT_CODE=$?
-echo
 
 if [ ${EXIT_CODE} == 0 ]; then
     echo "Backup of ${ANALYTICS_DB_NAME} is successfully completed";
--- scripts/builds/rpm/bahmni-mart-restore.sh
+++ scripts/builds/rpm/bahmni-mart-restore.sh
@@ -6,7 +6,6 @@ PATH_OF_CURRENT_SCRIPT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 . ${PATH_OF_CURRENT_SCRIPT}/bahmni-mart-db-details.sh
 
-read -p "Enter PEM file location (Optional, default is '~/.ssh/id_rsa.pub'): " PRIVATE_KEY_FILE_LOCATION
 read -p "Enter encrypted DB file location: " BACKUP_FILE_LOCATION
 
 if [ -z "$BACKUP_FILE_LOCATION" ]; then
@@ -14,13 +13,9 @@ if [ -z "$BACKUP_FILE_LOCATION" ]; then
     exit 1:
 fi
 
-if [ -z "$PRIVATE_KEY_FILE_LOCATION" ]; then
-    PRIVATE_KEY_FILE_LOCATION=~/.ssh/id_rsa.pub
-fi
-
 echo "Restoring  ${ANALYTICS_DB_NAME} with ${BACKUP_FILE_LOCATION}"
 
-openssl smime -decrypt -in ${BACKUP_FILE_LOCATION} -binary -inform DER -inkey ${PRIVATE_KEY_FILE_LOCATION} | bzcat | psql -h ${ANALYTICS_DB_HOST} -p ${POSTGRESQL_PORT} -U ${ANALYTICS_DB_USERNAME} ${ANALYTICS_DB_NAME} 1> /dev/null
+gpg --decrypt ${BACKUP_FILE_LOCATION} | psql -h ${ANALYTICS_DB_HOST} -p ${POSTGRESQL_PORT} -U ${ANALYTICS_DB_USERNAME} ${ANALYTICS_DB_NAME}
 
 EXIT_CODE=$?
 echo
