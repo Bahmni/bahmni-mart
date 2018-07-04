ada48cadfbeb90e981aa66431658fd5078487d1f
sumanmaity112 <suman.maity112@gmail.com>
2018-07-04 15:10:51 +0530

    Suman, Praveen | #94 | Add support to take unencrypted backup

--- scripts/builds/rpm/bahmni-mart.sh
+++ scripts/builds/rpm/bahmni-mart.sh
@@ -1,5 +1,23 @@
 #!/usr/bin/env bash
 
+display_help(){
+    printf "Usage:
+    bahmni-mart [OPTIONS]
+    By default it will launch bahmni-mart application
+    Options :
+        backup -e: To take encrypted db backup
+            Currently there is 2 way to take backup using bahmni-mart
+                i. csv - Take backup in Comma Separated Value(CSV) format
+                ii. sql - Take backup as sql dump file
+            If you don't provide any format, by default it will use 'sql' format
+        backup: To take unencrypted backup. The backup always will be 'sql' format
+        restore: To restore db using encrypted sql dump file
+        metabase-backup: To take metabase db backup. (only if metabase is installed)
+        import-key: To import public key of others
+        create-key: Create a new public/private key pair
+    RUN 'bahmni-mart --help' for more information on a command \n"
+}
+
 launch_bahmni_mart(){
     echo "Launching bahmni-mart..."
     nohup java -jar /opt/bahmni-mart/lib/bahmni-mart.jar --spring.config.location="/opt/bahmni-mart/properties/" >> /var/log/bahmni-mart/bahmni-mart.log 2>&1 &
@@ -7,7 +25,11 @@ launch_bahmni_mart(){
 }
 
 bahmni_mart_backup(){
-    sh /opt/bahmni-mart/bin/bahmni-mart-backup.sh
+    if [[ -z $1 || "$1" == "-e" ]]; then
+        sh /opt/bahmni-mart/bin/bahmni-mart-backup.sh $1
+    else
+        display_help;
+    fi
 }
 
 metabase_backup(){
@@ -26,27 +48,10 @@ create_new_key(){
     sh /opt/bahmni-mart/bin/generate-new-key.sh
 }
 
-display_help(){
-    printf "Usage:
-    bahmni-mart [OPTIONS]
-    By default it will launch bahmni-mart application
-    Options :
-        backup: To take encrypted db backup
-            Currently there is 2 way to take backup using bahmni-mart
-                i. csv - Take backup in Comma Separated Value(CSV) format
-                ii. sql - Take backup as sql dump file
-            If you don't provide any format, by default it will use 'sql' format
-        restore: To restore db using encrypted sql dump file
-        metabase-backup: To take metabase db backup. (only if metabase is installed)
-        import-key: To import public key of others
-        create-key: Create a new public/private key pair
-    RUN 'bahmni-mart --help' for more information on a command \n"
-}
-
 if [[ -z $1 ]]; then
     launch_bahmni_mart;
 elif [[ "$1" == "backup" ]]; then
-    bahmni_mart_backup;
+    bahmni_mart_backup $2;
 elif [[ "$1" == "metabase-backup" ]]; then
     metabase_backup;
 elif [[ "$1" == "import-key" ]]; then
