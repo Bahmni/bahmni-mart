a9ca5c8b6ba61896456f14d4b1bc449272b18c7d
Sneha Bagri <snehabagri90@gmail.com>
2019-01-15 13:23:22 +0530

    MOBN-221 | Fetch conepts for Form2 forms for flattening
    (cherry picked from commit 420638be7db47b71bebed0e62375183fa5a9b747)

--- src/main/java/org/bahmni/mart/job/JobContext.java
+++ src/main/java/org/bahmni/mart/job/JobContext.java
@@ -22,7 +22,7 @@ public class JobContext {
     private ObsJobStrategy obsJobStrategy;
 
     @Autowired
-    private Form2ObsJobStrategy form2ObsJobStrategy;
+    private Form2ObsJobStratergy form2ObsJobStratergy;
 
     @Autowired
     private EAVJobStrategy eavJobStrategy;
@@ -71,7 +71,7 @@ public class JobContext {
         Map<String, JobStrategy> strategyMap = new HashMap<>();
         strategyMap.put(JobType.CUSTOMSQL.toString().toLowerCase(), customSqlJobStrategy);
         strategyMap.put(JobType.OBS.toString().toLowerCase(), obsJobStrategy);
-        strategyMap.put(JobType.FORM2OBS.toString().toLowerCase(), form2ObsJobStrategy);
+        strategyMap.put(JobType.FORM2OBS.toString().toLowerCase(), form2ObsJobStratergy);
         strategyMap.put(JobType.EAV.toString().toLowerCase(), eavJobStrategy);
         strategyMap.put(JobType.BACTERIOLOGY.toString().toLowerCase(), bacteriologyJobStrategy);
         strategyMap.put(JobType.METADATA.toString().toLowerCase(), metaDataJobStrategy);
