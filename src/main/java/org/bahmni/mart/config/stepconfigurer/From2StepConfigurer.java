a17b267ab6aed761b13811805d50cf03e93d413c
Sneha Bagri <snehabagri90@gmail.com>
2019-01-16 11:00:41 +0530

    Alekhya, Sneha Bagri| MOBN-221: |Create table for form2
    (cherry picked from commit dac9ba8b74f33f3418d4a3abfa3ebcd023809ff4)

--- src/main/java/org/bahmni/mart/config/stepconfigurer/From2StepConfigurer.java
+++ src/main/java/org/bahmni/mart/config/stepconfigurer/From2StepConfigurer.java
@@ -4,9 +4,13 @@ import org.bahmni.mart.config.job.model.JobDefinition;
 import org.bahmni.mart.form.domain.BahmniForm;
 import org.bahmni.mart.form2.Form2ListProcessor;
 import org.bahmni.mart.form2.service.FormService;
-import org.bahmni.mart.form2.uitl.Form2MetadataReader;
 import org.bahmni.mart.helper.FormListHelper;
+import org.bahmni.mart.table.Form2TableMetadataGenerator;
+import org.bahmni.mart.table.FormTableMetadataGenerator;
+import org.bahmni.mart.table.TableMetadataGenerator;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
@@ -24,6 +28,11 @@ public class From2StepConfigurer extends StepConfigurer {
 
     @Autowired
     protected Form2ListProcessor form2ListProcessor;
+
+    @Autowired
+    public From2StepConfigurer(Form2TableMetadataGenerator formTableMetadataGenerator){
+        super(formTableMetadataGenerator);
+    }
 
     @Override
     protected List<BahmniForm> getAllForms() {
