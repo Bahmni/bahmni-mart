d6262c235527acb65eca967b45a22642402a68d7
sowmika <sowmika148@gmail.com>
2019-08-13 11:03:58 +0530

    Sowmika, Vinay | MOBN-636 | Fix partial write of obs data for forms2

--- src/test/java/org/bahmni/mart/form2/Form2ObservationProcessorTest.java
+++ src/test/java/org/bahmni/mart/form2/Form2ObservationProcessorTest.java
@@ -1,133 +1,64 @@
 package org.bahmni.mart.form2;
 
+import org.bahmni.mart.config.job.model.JobDefinition;
 import org.bahmni.mart.form.domain.BahmniForm;
 import org.bahmni.mart.form.domain.Concept;
 import org.bahmni.mart.form.domain.Obs;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mock;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import static org.bahmni.mart.CommonTestHelper.setValuesForMemberFields;
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNull;
+import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
+import static org.mockito.MockitoAnnotations.initMocks;
 
 public class Form2ObservationProcessorTest {
 
     private Form2ObservationProcessor form2ObservationProcessor;
 
+    @Mock
+    private JobDefinition jobDefinition;
+
+    @Mock
+    private NamedParameterJdbcTemplate jdbcTemplate;
+
     @Before
     public void setUp() throws Exception {
+        initMocks(this);
         form2ObservationProcessor = new Form2ObservationProcessor();
+        setValuesForMemberFields(form2ObservationProcessor, "jdbcTemplate", jdbcTemplate);
     }
-
-    @Test
-    public void shouldProcessAndConvertTheGivenDataToObs() {
-        BahmniForm form = mock(BahmniForm.class);
-        when(form.getDepthToParent()).thenReturn(0);
-        form2ObservationProcessor.setForm(form);
-        Map<String, Object> obsRow = getObsRow();
-        Map<String, String> fieldNameAndFullySpecifiedNameMap = new HashMap<>();
-        fieldNameAndFullySpecifiedNameMap.put("age", "age");
-        when(form.getFieldNameAndFullySpecifiedNameMap()).thenReturn(fieldNameAndFullySpecifiedNameMap);
-
-        List<Obs> obsList = form2ObservationProcessor.process(obsRow);
-
-        assertEquals(1, obsList.size());
-        Obs obs = obsList.get(0);
-        assertEquals("abc123", obs.getEncounterId());
-        assertEquals("def789", obs.getPatientId());
-        assertEquals(Integer.valueOf(97), obs.getId());
-        Concept field = obs.getField();
-        assertEquals(Integer.valueOf(7), field.getId());
-        assertEquals("age", field.getName());
-        assertEquals("21", obs.getValue());
-        assertEquals("2015-02-01 00:00:00", obs.getObsDateTime());
-        assertEquals("2016-11-09 7:31:43", obs.getDateCreated());
-        assertEquals("8", obs.getLocationId());
-        assertEquals("Registration", obs.getLocationName());
-        assertEquals("1", obs.getProgramId());
-        assertEquals("First Stage Validation", obs.getProgramName());
-    }
-
-    @Test
-    public void shouldProcessGivenDataToCodedObs() {
-        BahmniForm form = mock(BahmniForm.class);
-        when(form.getDepthToParent()).thenReturn(0);
-        form2ObservationProcessor.setForm(form);
-        Map<String, Object> obsRow = getCodedObsRow();
-        Map<String, String> fieldNameAndFullySpecifiedNameMap = new HashMap<>();
-        fieldNameAndFullySpecifiedNameMap.put("Coded concept", "Coded concept in Given locale");
-        fieldNameAndFullySpecifiedNameMap.put("Concept Answer", "Concept Answer in Given Locale");
-        when(form.getFieldNameAndFullySpecifiedNameMap()).thenReturn(fieldNameAndFullySpecifiedNameMap);
-
-        List<Obs> obsList = form2ObservationProcessor.process(obsRow);
-
-        assertEquals(1, obsList.size());
-        Obs obs = obsList.get(0);
-        assertEquals("abc123", obs.getEncounterId());
-        assertEquals("def789", obs.getPatientId());
-        assertEquals(Integer.valueOf(97), obs.getId());
-        Concept field = obs.getField();
-        assertEquals(Integer.valueOf(7), field.getId());
-        assertEquals("Coded concept in Given locale", field.getName());
-        assertEquals("Concept Answer in Given Locale", obs.getValue());
-        assertEquals("2015-02-01 00:00:00", obs.getObsDateTime());
-        assertEquals("2016-11-09 7:31:43", obs.getDateCreated());
-        assertEquals("8", obs.getLocationId());
-        assertEquals("Registration", obs.getLocationName());
-        assertEquals("1", obs.getProgramId());
-        assertEquals("First Stage Validation", obs.getProgramName());
-    }
-
-    @Test
-    public void shouldReturnObsWithParentFormFieldPathWhenFormDepthToParentIsZero() {
-        BahmniForm form = mock(BahmniForm.class);
-        when(form.getDepthToParent()).thenReturn(0);
-        form2ObservationProcessor.setForm(form);
-        Map<String, Object> obsRow = getObsRow();
-
-        List<Obs> obsList = form2ObservationProcessor.process(obsRow);
-
-        assertEquals(1, obsList.size());
-        Obs obs = obsList.get(0);
-        assertEquals("FormOne", obs.getFormFieldPath());
-        assertNull(obs.getReferenceFormFieldPath());
-    }
-
-    @Test
-    public void shouldReturnObsWithParentFormFieldPathAndReferenceFormFieldPathWhenFormDepthToParentIs2() {
-        BahmniForm form = mock(BahmniForm.class);
-        BahmniForm parentForm = mock(BahmniForm.class);
-        when(parentForm.getDepthToParent()).thenReturn(0);
-        when(form.getParent()).thenReturn(parentForm);
-        when(form.getDepthToParent()).thenReturn(2);
-        form2ObservationProcessor.setForm(form);
-        Map<String, Object> obsRow = getObsRow();
-
-        List<Obs> obsList = form2ObservationProcessor.process(obsRow);
-
-        assertEquals(1, obsList.size());
-        Obs obs = obsList.get(0);
-        assertEquals("FormOne.1/3-0/7-0", obs.getFormFieldPath());
-        assertEquals("FormOne", obs.getReferenceFormFieldPath());
-    }
-
 
     @Test
     public void shouldReturnObsWithParentFormFieldPathAndReferenceFormFieldPathWhenParentFormDepthToParentIs1() {
         BahmniForm form = mock(BahmniForm.class);
+        BahmniForm rootForm = new BahmniForm();
+        rootForm.setFormName(new Concept(1, "FormOne", 0));
+        when(form.getRootForm()).thenReturn(rootForm);
         BahmniForm parentForm = mock(BahmniForm.class);
+        when(parentForm.getDepthToParent()).thenReturn(1);
         when(parentForm.getDepthToParent()).thenReturn(1);
         when(form.getParent()).thenReturn(parentForm);
         when(form.getDepthToParent()).thenReturn(3);
+        when(jobDefinition.getConceptReferenceSource()).thenReturn(null);
         form2ObservationProcessor.setForm(form);
-        Map<String, Object> obsRow = getObsRow();
+        form2ObservationProcessor.setJobDefinition(jobDefinition);
+        Map<String, Object> encounterIdMap = new HashMap<>();
+        encounterIdMap.put("encounter_id", "abc123");
+        when(jdbcTemplate.query(any(), any(Map.class), any(BeanPropertyRowMapper.class)))
+                .thenReturn(Collections.singletonList(getObsRow()));
 
-        List<Obs> obsList = form2ObservationProcessor.process(obsRow);
+        List<Obs> obsList = form2ObservationProcessor.process(encounterIdMap);
 
         assertEquals(1, obsList.size());
         Obs obs = obsList.get(0);
@@ -135,57 +66,21 @@ public class Form2ObservationProcessorTest {
         assertEquals("FormOne.1/3-0", obs.getReferenceFormFieldPath());
     }
 
-    @Test
-    public void shouldReturnObsWithParentFormFieldPathAndReferenceFormFieldPathWhenFormIsMultiSelect() {
-        BahmniForm form = mock(BahmniForm.class);
-        BahmniForm parentForm = mock(BahmniForm.class);
-        when(parentForm.getDepthToParent()).thenReturn(1);
-        when(form.getParent()).thenReturn(parentForm);
-        when(form.getDepthToParent()).thenReturn(4);
-        form2ObservationProcessor.setForm(form);
-        Map<String, Object> obsRow = getObsRow();
-
-        List<Obs> obsList = form2ObservationProcessor.process(obsRow);
-
-        assertEquals(1, obsList.size());
-        Obs obs = obsList.get(0);
-        assertEquals("FormOne.1/3-0/7-0/2-1/100-0", obs.getFormFieldPath());
-        assertEquals("FormOne.1/3-0", obs.getReferenceFormFieldPath());
-    }
-
-    private Map<String, Object> getObsRow() {
-        Map<String, Object> obsRow = new HashMap<>();
-        obsRow.put("encounter_id", "abc123");
-        obsRow.put("patientId", "def789");
-        obsRow.put("conceptId", 7);
-        obsRow.put("id", 97);
-        obsRow.put("value", "21");
-        obsRow.put("conceptName", "age");
-        obsRow.put("obsDateTime", "2015-02-01 00:00:00");
-        obsRow.put("dateCreated", "2016-11-09 7:31:43");
-        obsRow.put("locationId", "8");
-        obsRow.put("locationName", "Registration");
-        obsRow.put("programId", "1");
-        obsRow.put("programName", "First Stage Validation");
-        obsRow.put("formFieldPath", "FormOne.1/3-0/7-0/2-1/100-0");
-        return obsRow;
-    }
-
-    private Map<String, Object> getCodedObsRow() {
-        Map<String, Object> obsRow = new HashMap<>();
-        obsRow.put("encounter_id", "abc123");
-        obsRow.put("patientId", "def789");
-        obsRow.put("conceptId", 7);
-        obsRow.put("id", 97);
-        obsRow.put("value", "Concept Answer");
-        obsRow.put("conceptName", "Coded concept");
-        obsRow.put("obsDateTime", "2015-02-01 00:00:00");
-        obsRow.put("dateCreated", "2016-11-09 7:31:43");
-        obsRow.put("locationId", "8");
-        obsRow.put("locationName", "Registration");
-        obsRow.put("programId", "1");
-        obsRow.put("programName", "First Stage Validation");
-        obsRow.put("formFieldPath", "FormOne.1/3-0/7-0/2-1/100-0");
+    private Obs getObsRow() {
+        Obs obsRow = new Obs();
+        obsRow.setEncounterId("abc123");
+        obsRow.setPatientId("def789");
+        obsRow.setField(new Concept(7,"age", 0));
+        obsRow.setId(97);
+        obsRow.setValue("21");
+        obsRow.setObsDateTime("2015-02-01 00:00:00");
+        obsRow.setDateCreated("2016-11-09 7:31:43");
+        obsRow.setLocationId("8");
+        obsRow.setLocationName("Registration");
+        obsRow.setProgramId("1");
+        obsRow.setProgramName("First Stage Validation");
+        obsRow.setFormFieldPath("FormOne.1/3-0/7-0/2-1");
+        obsRow.setReferenceFormFieldPath("FormOne.1/3-0");
         return obsRow;
     }
 }
