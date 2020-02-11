2dcd13a356e7fc69840f58f551c4212eab9a0041
binduak <tbindu@thoughtworks.com>
2020-02-11 13:18:03 +0530

    Bindu | MOBN-1091 | Move the build version check pre-push logic to existing scripts/dev/pre-push file

--- scripts/dev/pre-push.sh
+++ scripts/dev/pre-push.sh
@@ -13,6 +13,23 @@ if [ "$isStashed" ]
     git stash pop
 fi
 
+versionLineNumber=`( grep -n "version =" build.gradle | cut -d' ' -f1 | sed 's/^\(..\).*/\1/')`
+lineCommit=`(git log  -L $versionLineNumber,$versionLineNumber:build.gradle --max-count=1 --pretty=format:"%H" | head -n 1)`
+currentBranch=`git branch | sed -n '/\* /s///p'`
+if [ $currentBranch == "master" ]; then
+        git cherry -v origin/$currentBranch | cut -d' ' -f2 | tee localCommits.txt
+        else
+        exit 0;
+fi
+if [ `grep $lineCommit localCommits.txt`  ]; then
+        rm -rf localCommits.txt
+        exit 0;
+        else
+        echo "Please update bahmni-mart build version with every git push to master branch"
+        rm -rf localCommits.txt
+        exit 1;
+fi
+
 if [ ${STATUS} != 0 ]
 then
 	exit 1
