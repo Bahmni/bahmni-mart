07b44edc4ba857a89a6a3a9381c00f888f5a77cf
venkatavineela <vineela.vini421.vv@gmail.com>
2020-02-24 17:55:28 +0530

    Vineela | MOBN-1051 | Add program_id in medication_data_default, patient_visit_details_default, patient_encounter_details_default, patient_visits_encounters_view tables

--- src/main/resources/viewSql/patientVisitsEncountersView.sql
+++ src/main/resources/viewSql/patientVisitsEncountersView.sql
@@ -1,5 +1,5 @@
 SELECT
-  pd.person_id,
+  pvd.patient_id,
   pd.gender,
   pd.birthyear,
   EXTRACT(YEAR FROM (SELECT age( pvd.visit_start_date, TO_DATE(CONCAT('01-01-', pd.birthyear), 'dd-MM-yyyy')))) AS age_at_visit,
@@ -15,7 +15,8 @@ SELECT
   encounter_id,
   encounter_type_id,
   encounter_type_name,
-  ped.location_name AS encounter_location
+  ped.location_name AS encounter_location,
+  pvd.program_id    AS program_id
 
 FROM person_details_default pd INNER JOIN patient_visit_details_default pvd ON pvd.patient_id = pd.person_id
                              INNER JOIN patient_encounter_details_default ped ON ped.patient_id = pvd.patient_id
