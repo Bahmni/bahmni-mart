7f224266979f954c7a4bb28ec4db037d1e74e6c4
Himabindu T <tbindu@thoughtworks.com>
2020-02-25 11:15:02 +0530

    Bindu | MOBN-1049 | Discharge date getting populated in bed_management_locations_view table even when the patient isn't discharged yet (#23)
    * Bindu | MOBN-1049 | Discharge date getting populated in bed_management_locations_view table even when the patient isn't discharged yet
    
    * Bindu | MOBN-1049 | Fixing a typo

--- src/main/resources/viewSql/locationWiseDischarge.sql
+++ src/main/resources/viewSql/locationWiseDischarge.sql
@@ -3,7 +3,7 @@ SELECT
   bpam.visit_id,
   bpam.location,
   min(bpam.date_started)              AS start_date,
-  coalesce(discharge_date(patient_id,bpam.location,date_stopped),current_date) AS discharge_date
+  coalesce(discharge_date(patient_id,bpam.location,date_stopped),null) AS discharge_date
 FROM bed_patient_assignment_default bpam
 GROUP BY patient_id,visit_id,discharge_date,location
 ORDER BY start_date,discharge_date
