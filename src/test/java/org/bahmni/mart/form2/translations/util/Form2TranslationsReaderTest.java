9e73c47635560819039ea3670cde9375416c2eff
venkatavineela <vineela.vini421.vv@gmail.com>
2020-05-20 18:12:42 +0530

    Bindu, Vineela | MOBN-1268 | Add support to get translations from files with UUID as file name

--- src/test/java/org/bahmni/mart/form2/translations/util/Form2TranslationsReaderTest.java
+++ src/test/java/org/bahmni/mart/form2/translations/util/Form2TranslationsReaderTest.java
@@ -68,6 +68,12 @@ public class Form2TranslationsReaderTest {
         file.createNewFile();
     }
 
+    private void createFileWithUuid(String uuidFileName) throws IOException {
+        File file = new File(tempTranslationFolderPath + "/" + uuidFileName);
+        file.createNewFile();
+    }
+
+
     @Test
     public void shouldReturnForm2TranslationsForGivenFormNameVersionAndLocale() throws IOException {
 
@@ -93,7 +99,8 @@ public class Form2TranslationsReaderTest {
                 "    }\n" +
                 "  }\n" +
                 "}";
-
+        when(translationMetadata.getTranslationsFilePathWithUuid(formName, formVersion))
+                .thenReturn(tempTranslationFolderPath+"/"+".json");
         when(translationMetadata.getTranslationsFilePath(formName, formVersion))
                 .thenReturn(tempTranslationFolderPath + "/" + formName);
         when(readFileToString(any(File.class))).thenReturn(translationsAsString);
@@ -111,7 +118,6 @@ public class Form2TranslationsReaderTest {
     @Test
     public void shouldReturnForm2TranslationsForGivenSpecialCharacterFormNameVersionAndLocaleWithNormalizedFilePath()
             throws IOException {
-
         String formNameWithSpecialCharacters = "2 Vitãls spéciælity_2.json";
         String normalizedFileName = "2_Vit_ls_sp_ci_lity_2.json";
         createFile(normalizedFileName);
@@ -135,7 +141,8 @@ public class Form2TranslationsReaderTest {
                 "    }\n" +
                 "  }\n" +
                 "}";
-
+        when(translationMetadata.getTranslationsFilePathWithUuid(formNameWithSpecialCharacters, formVersion))
+                .thenReturn(tempTranslationFolderPath+"/"+".json");
         when(translationMetadata.getTranslationsFilePath(formNameWithSpecialCharacters, formVersion))
                 .thenReturn(tempTranslationFolderPath + "/" + formNameWithSpecialCharacters);
         when(translationMetadata.getNormalizedTranslationsFilePath(formNameWithSpecialCharacters, formVersion))
@@ -153,14 +160,62 @@ public class Form2TranslationsReaderTest {
 
         InOrder inOrder = inOrder(translationMetadata);
         inOrder.verify(translationMetadata, times(1))
+                .getTranslationsFilePathWithUuid(formNameWithSpecialCharacters, formVersion);
+        inOrder.verify(translationMetadata, times(1))
                 .getTranslationsFilePath(formNameWithSpecialCharacters, formVersion);
         inOrder.verify(translationMetadata, times(1))
                 .getNormalizedTranslationsFilePath(formNameWithSpecialCharacters, formVersion);
     }
 
     @Test
+    public void shouldReturnForm2TranslationsForGivenFormNameVersionAndLocaleWhenTranslationsFileNameIsUuid() throws IOException {
+
+        String formName = "Vitals_2.json";
+        String uuidFileName = "\"91770617-a6d0-4ad4-a0a2-c77bd5926bd2.json\"";
+        createFileWithUuid(uuidFileName);
+        String translationsAsString = "{\n" +
+                "  \"en\": {\n" +
+                "    \"concepts\": {\n" +
+                "      \"HEIGHT_1\": \"Height\",\n" +
+                "      \"WEIGHT_1\": \"Weight\",\n" +
+                "    },\n" +
+                "    \"labels\": {\n" +
+                "      \"SECTION_1\": \"Readings\"\n" +
+                "    }\n" +
+                "  },\n" +
+                "  \"fr\": {\n" +
+                "    \"concepts\": {\n" +
+                "      \"HEIGHT_1\": \"la taille\",\n" +
+                "      \"WEIGHT_1\": \"Poids\",\n" +
+                "    },\n" +
+                "    \"labels\": {\n" +
+                "      \"SECTION_1\": \"Lectures\"\n" +
+                "    }\n" +
+                "  }\n" +
+                "}";
+
+
+        when(translationMetadata.getTranslationsFilePathWithUuid(formName, formVersion))
+                .thenReturn(tempTranslationFolderPath + "/" + uuidFileName);
+        when(readFileToString(any(File.class))).thenReturn(translationsAsString);
+
+        Form2Translation form2Translations = form2TranslationsReader.read(formName, formVersion, locale);
+
+        assertEquals(2, form2Translations.getConcepts().size());
+        assertEquals(1, form2Translations.getLabels().size());
+        assertEquals("la taille", form2Translations.getConcepts().get("HEIGHT_1"));
+        assertEquals("Poids", form2Translations.getConcepts().get("WEIGHT_1"));
+        assertEquals("Lectures", form2Translations.getLabels().get("SECTION_1"));
+
+    }
+
+    @Test
     public void shouldLogWarningIfTranslationsFileNotFound() throws Exception {
         String invalidFilePath = "abc/Vitals_2.json";
+        String invalidFilePathWithUuid = "abc/91770617-a6d0-4ad4-a0a2-c77bd5926bd2.json";
+
+        when(translationMetadata.getTranslationsFilePathWithUuid(formName, formVersion))
+                .thenReturn(invalidFilePathWithUuid);
         when(translationMetadata.getTranslationsFilePath(formName, formVersion))
                 .thenReturn(invalidFilePath);
         when(translationMetadata.getNormalizedTranslationsFilePath(formName, formVersion))
@@ -178,6 +233,11 @@ public class Form2TranslationsReaderTest {
     @Test
     public void shouldLogWarningForAnyIOException() throws Exception {
         String translationFilePath = tempTranslationFolderPath + "/Vitals_2.json";
+        String translationFilePathWithUuid = tempTranslationFolderPath + "/91770617-a6d0-4ad4-a0a2-c77bd5926bd2.json";
+
+
+        when(translationMetadata.getTranslationsFilePathWithUuid(formName, formVersion))
+                .thenReturn(translationFilePathWithUuid);
         when(translationMetadata.getTranslationsFilePath(formName, formVersion))
                 .thenReturn(translationFilePath);
         when(translationMetadata.getNormalizedTranslationsFilePath(formName,formVersion))
