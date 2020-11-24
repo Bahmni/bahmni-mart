7b8fcec5035402a64571a5937d8be9d67a2a719f
Himabindu T <tbindu@thoughtworks.com>
2020-11-24 11:32:29 +0530

    Bindu | MOBN-1527 | Remove program_id from the visit and enounter specific tables (#34)

--- src/main/resources/viewSql/patientVisitsEncountersView.sql
+++ src/main/resources/viewSql/patientVisitsEncountersView.sql
@@ -15,8 +15,7 @@ SELECT
   encounter_id,
   encounter_type_id,
   encounter_type_name,
-  ped.location_name AS encounter_location,
-  pvd.program_id    AS program_id
+  ped.location_name AS encounter_location
 
 FROM person_details_default pd INNER JOIN patient_visit_details_default pvd ON pvd.patient_id = pd.person_id
                              INNER JOIN patient_encounter_details_default ped ON ped.patient_id = pvd.patient_id
