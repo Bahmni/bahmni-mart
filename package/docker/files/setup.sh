0c59d7fba16bdc453017a340f656c573d2a29f29
pajmera4 <pajmera@thoughtworks.com>
2022-12-22 14:53:23 +0530

    BAH-2582 Add. Dockerfile to create Bahmni Mart docker image

---
+++ package/docker/files/setup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+printenv > /etc/cron.d/crontab
+echo "$CRON_TIME \
+java -jar /bahmni-mart/app.jar --spring.config.location='/bahmni-mart/application.properties' \
+> /proc/1/fd/1 2>/proc/1/fd/2" >> /etc/cron.d/crontab
+
+echo "Setting Cron to Run Jar"
+crontab /etc/cron.d/crontab
+echo "Running Cron Job"
+cron -f
